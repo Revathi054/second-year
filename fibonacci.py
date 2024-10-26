4ed957e92a8d004adbcb20e485915782fcd94e1e
RevathiKillampalli <revathi3634@gmail.com>
2024-10-26 20:50:46 +0530

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,9 +1,8 @@
 def fibonacci(n):
-    if n <= 1:
+    if n == 1 or n==0:
         return n
     return fibonacci(n - 1) + fibonacci(n - 2)
 
-# Test cases
 print(fibonacci(0))  # Expected: 0
 print(fibonacci(5))  # Expected: 5
 print(fibonacci(10)) # Expected: 55
